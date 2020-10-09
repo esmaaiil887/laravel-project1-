af56a94607b873797ae2be5d7139f44df257fa9b
unknown <esmaaiil887@gmail.com>
2020-10-10 01:43:18 +0200

    upload laravel files

---
+++ app/resturant.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class resturant extends Model
+{
+    //
+
+  protected  $table="products";
+  protected  $primaryKey = 'products_id ';
+  public $timesstamps=false;
+
+
+
+
+
+
+}
